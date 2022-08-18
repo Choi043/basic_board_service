ddd82df1de0405b06dbb218bacbee2db895d975c
Choi043 <rqdda11@gmail.com>
2022-08-19 07:11:46 +0900

    DB connections & settings

--- src/main/java/com/example/basic_board_project/controller/ArticleController.java
+++ src/main/java/com/example/basic_board_project/controller/ArticleController.java
@@ -1,12 +1,18 @@
 package com.example.basic_board_project.controller;
 
 import com.example.basic_board_project.dto.ArticleForm;
+import com.example.basic_board_project.entity.Article;
+import com.example.basic_board_project.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -16,6 +22,13 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticles(ArticleForm form) {
         System.out.println(form.toString());
+
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
+
         return "";
     }
 }
--- src/main/java/com/example/basic_board_project/dto/ArticleForm.java
+++ src/main/java/com/example/basic_board_project/dto/ArticleForm.java
@@ -1,5 +1,7 @@
 package com.example.basic_board_project.dto;
 
+import com.example.basic_board_project.entity.Article;
+
 public class ArticleForm {
 
     private String title;
@@ -16,5 +18,9 @@ public class ArticleForm {
                 "title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 '}';
+    }
+
+    public Article toEntity() {
+        return new Article(null, title, content);
     }
 }
