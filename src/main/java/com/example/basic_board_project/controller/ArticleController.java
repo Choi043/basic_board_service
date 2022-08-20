5a1929d93049a36511d50969c1a32a4932edd2e0
Choi043 <rqdda11@gmail.com>
2022-08-21 03:01:06 +0900

    refactoring to lombok

--- src/main/java/com/example/basic_board_project/controller/ArticleController.java
+++ src/main/java/com/example/basic_board_project/controller/ArticleController.java
@@ -3,12 +3,15 @@ package com.example.basic_board_project.controller;
 import com.example.basic_board_project.dto.ArticleForm;
 import com.example.basic_board_project.entity.Article;
 import com.example.basic_board_project.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class ArticleController {
 
     @Autowired
@@ -21,13 +24,13 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticles(ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
 
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
 
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
 
         return "";
     }
