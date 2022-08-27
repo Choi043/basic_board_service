1a58bb5b217db80f7bc3931e2db973fdc0e92e3c
Choi043 <rqdda11@gmail.com>
2022-08-28 00:02:22 +0900

    add RestAPI - Patch Method

--- src/main/java/com/example/basic_board_project/api/ArticleApiController.java
+++ src/main/java/com/example/basic_board_project/api/ArticleApiController.java
@@ -3,11 +3,14 @@ package com.example.basic_board_project.api;
 import com.example.basic_board_project.dto.ArticleForm;
 import com.example.basic_board_project.entity.Article;
 import com.example.basic_board_project.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 public class ArticleApiController {
     @Autowired // DI
@@ -29,5 +32,27 @@ public class ArticleApiController {
     public Article create(@RequestBody ArticleForm dto) {
         Article article = dto.toEntity();
         return articleRepository.save(article);
+    }
+
+    // Patch
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id,
+                                          @RequestBody ArticleForm dto) {
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 잘못된 요청 처리
+        if(target == null || id != article.getId()) {
+            // 400
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        // 수정 및 정상 응답(200)
+        target.patch(article);      // 기존 article에 붙여줌, 즉 body에 누락되면 기존 값으로 대체
+        Article updated = articleRepository.save(target);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
     }
 }
