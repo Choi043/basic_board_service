150e39f64124a399234562630cef83a5943a9b2c
Choi043 <rqdda11@gmail.com>
2022-08-27 22:28:24 +0900

    add RestAPI - Post Method

--- src/main/java/com/example/basic_board_project/api/ArticleApiController.java
+++ src/main/java/com/example/basic_board_project/api/ArticleApiController.java
@@ -1,11 +1,10 @@
 package com.example.basic_board_project.api;
 
+import com.example.basic_board_project.dto.ArticleForm;
 import com.example.basic_board_project.entity.Article;
 import com.example.basic_board_project.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,12 @@ public class ArticleApiController {
     @GetMapping("/api/articles/{id}")
     public Article index(@PathVariable Long id) {
         return articleRepository.findById(id).orElse((null));
+    }
+
+    // Post
+    @PostMapping("/api/articles")
+    public Article create(@RequestBody ArticleForm dto) {
+        Article article = dto.toEntity();
+        return articleRepository.save(article);
     }
 }
