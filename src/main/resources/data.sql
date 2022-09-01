8f444db9c4d7485a88586864e5de83200b2c6053
Choi043 <rqdda11@gmail.com>
2022-09-01 22:49:32 +0900

    add SQL dummy data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
-INSERT INTO article(id, title, content) VALUES (1, '가가가가', '1111');
-INSERT INTO article(id, title, content) VALUES (2, '나나나나', '2222');
-INSERT INTO article(id, title, content) VALUES (3, '다다다다', '3333');
+-- 게시판 더미 데이터
+INSERT INTO article(id, title, content) VALUES(1, '가가가가', '1111');
+INSERT INTO article(id, title, content) VALUES(2, '나나나나', '2222');
+INSERT INTO article(id, title, content) VALUES(3, '다다다다', '3333');
+
+-- 게시글 더미 데이터
+INSERT INTO article(id, title, content) VALUES(4, '당신의 인생 영화는?', '댓글 ㄱ');
+INSERT INTO article(id, title, content) VALUES(5, '당신의 소울 푸드는?', '댓글 ㄱㄱ');
+INSERT INTO article(id, title, content) VALUES(6, '당신의 취미는?', '댓글 ㄱㄱㄱ');
