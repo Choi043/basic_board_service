e75f80d83cacf3826482fc7b91c61b85f4713774
Choi043 <rqdda11@gmail.com>
2022-09-02 00:33:09 +0900

    add comment Entity

---
+++ src/main/java/com/example/basic_board_project/entity/comment.java
@@ -0,0 +1,29 @@
+package com.example.basic_board_project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String body;
+}
